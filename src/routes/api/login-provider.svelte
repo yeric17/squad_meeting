ff8dab9a910f0bf448ee7953e85cc5c34ba16bf8
Carlos Yeric Fonseca Rios <Carlos.Rios@ipsos.com>
2022-07-20 00:02:38 -0500

    fix redirec in login-provider

--- src/routes/api/login-provider.svelte
+++ src/routes/api/login-provider.svelte
@@ -9,7 +9,7 @@
     //const SUPABASE_JWT = import.meta.env.VITE_PUBLIC_SUPABASE_JWT;
     onMount(async()=>{
         const sectionsURL =  document.URL.replace('#','').split("/")
-        const newURL = sectionsURL[sectionsURL.length - 1].replace('login-google?','')
+        const newURL = sectionsURL[sectionsURL.length - 1].replace('login-provider?','')
 
         const reaponse = await fetch(`${API_HOST}/decode-token?${newURL}`,{method:'POST'})
         
